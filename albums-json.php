6f755661596f3bd059363b52cae013208b8042e7
ketjj <jorjolianiket@gmail.com>
2022-06-09 16:19:03 +0200

    init

---
+++ albums-json.php
@@ -0,0 +1,65 @@
+<?php
+
+$albums = [
+  [
+    'image' =>"https://m.media-amazon.com/images/I/51sEXKRYYDL._AC_SY355_.jpg",
+    'title' =>"New Jersey",
+    'artist' =>"Bon Jovi",
+    'releaseYear' => 1988
+  ],
+  [
+    'image' =>"https://m.media-amazon.com/images/I/51HxWMVROtL._AC_SX425_.jpg",
+    'title' =>"Live at Wembley 86",
+    'artist' =>"Queen",
+    'releaseYear' => 1992
+  ],
+  [
+    'image' =>"https://images-eu.ssl-images-amazon.com/images/I/41JD3CW65HL.__AC_QL70_ML2_.jpg",
+    'title' =>"Ten's Summoner's Tales",
+    'artist' =>"Sting",
+    'releaseYear' => 1993
+  ],
+  [
+    'image' =>"https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg",
+    'title' =>"Steve Gadd Band",
+    'artist' =>"Steve Gadd Band",
+    'releaseYear' => 2018
+  ],
+  [
+    'image' =>"https://m.media-amazon.com/images/I/716+UhNVyLL._AC_SL1500_.jpg",
+    'title' =>"Brave New World",
+    'artist' =>"Iron Maiden",
+    'releaseYear' => 2000
+  ],
+  [
+    'image' =>"https://m.media-amazon.com/images/I/81MDAIdh78L._AC_SX522_.jpg",
+    'title' =>"One more car, one more raider",
+    'artist' =>"Eric Clapton ",
+    'releaseYear' => 2002
+  ],
+  [
+    'image' =>"https://m.media-amazon.com/images/I/51rggtPgmRL._AC_SY355_.jpg",
+    'title' =>"made in Japan",
+    'artist' =>"Deep Purple",
+    'releaseYear' => 1972
+  ],
+  [
+    'image' =>"https://m.media-amazon.com/images/I/51ZnAP3vmZL._SY1000_.jpg",
+    'title' =>"And Justice for all",
+    'artist' =>"Metallica",
+    'releaseYear' => 1988
+  ],
+  [
+    'image' =>"https://i.scdn.co/image/ab67616d0000b2731cbb937b5dc5c3638a42cbc2",
+    'title' =>"Hard Wired",
+    'artist' =>"Dave Wecki",
+    'releaseYear' => 1994
+  ],
+  [
+    'image' =>"https://m.media-amazon.com/images/I/6148jblbNqL._AC_SX679_.jpg",
+    'title' =>"Bad",
+    'artist' =>"Michael Jackson",
+    'releaseYear' => 1987
+  ]
+];
+
