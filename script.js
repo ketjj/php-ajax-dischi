2a62a228aabc5fc356e9725ae5d44e0cdad98dd6
ketjj <jorjolianiket@gmail.com>
2022-06-09 19:47:56 +0200

    milestone 2

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const app = new Vue({
   el: '#app',
-  data:{},
-  mounted(){},
-  methods: {},
+
+  data:{
+    albums: [],
+    apiURL: 'http://localhost/php/php-ajax-dischi/api.php',
+  },
+
+  mounted(){
+    this.getApi();
+  },
+
+  methods: {
+    getApi(){
+      axios.get(this.apiURL)
+      .then(r =>{
+        this.albums = r.data;
+        console.log(r.data)
+      })
+    }
+  },
 })
